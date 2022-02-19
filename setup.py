0b4ac8a35c2e1e122c1311678b3754a1e593579b
Jakob McBroome <jmcbroome@gmail.com>
2022-02-19 14:05:12 -0800

    clean up environmental variables, use conda environment

--- setup.py
+++ setup.py
@@ -1,17 +1,21 @@
 from setuptools import Extension, setup
 from Cython.Build import cythonize
 import subprocess
+from os import environ
 
-#build the tbb library. It comes with a makefile prepared.
-subprocess.run("make -j",cwd="./dependencies/tbb/build",shell=True,check=True)
+CONDA_PREFIX = environ.get('CONDA_PREFIX', None)
+if CONDA_PREFIX == None:
+    print("Can't find conda installation- aborting.")
+    exit(1)
+
 #build the matree protoc reader files for this system and put them in the src
 subprocess.run("protoc --cpp_out=./src parsimony.proto",shell=True,check=True)
 
 extensions = [Extension("mat",["src/mat.pyx"],
-    include_dirs=["${CONDA_PREFIX}/include/google/protobuf", "${CONDA_PREFIX}/include/", "./dependencies/oneTBB/include"],
-    library_dirs=['${CONDA_PREFIX}/lib/','./dependencies/oneTBB/build/'],
+    include_dirs=[CONDA_PREFIX+"/include/google/protobuf", CONDA_PREFIX+"/include/", CONDA_PREFIX+"include/boost/iostreams", "dependencies/oneTBB/include"],
+    library_dirs=[CONDA_PREFIX+'/lib/','dependencies/oneTBB/build'],
     libraries=['tbb', 'protobuf','boost_system','boost_iostreams'],
     language='c++'
-    )]
+    )]  
 
 setup(name='mat',ext_modules=cythonize(extensions,language_level=3))
