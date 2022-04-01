8a2400981a9d1005dc094a831921718d3947de73
Jakob McBroome <jmcbroome@gmail.com>
2022-04-01 14:55:06 -0700

    update to run test script from main directory

--- test/test.py
+++ test/test.py
@@ -3,15 +3,15 @@ import unittest
 
 class TestMat(unittest.TestCase):
     def test_ps(self):
-        t = mat.MATree("test.pb")
+        t = mat.MATree("test/test.pb")
         ps = t.get_parsimony_score()
         self.assertTrue(ps > 0)
     def test_dfe(self):
-        t = mat.MATree("test.pb")
+        t = mat.MATree("test/test.pb")
         nodes = t.depth_first_expansion()
         self.assertTrue(len(nodes) > 0)
     def test_node(self):
-        t = mat.MATree("test.pb")
+        t = mat.MATree("test/test.pb")
         root = t.get_root()
         children = root.children
         self.assertTrue(len(children) > 0)
