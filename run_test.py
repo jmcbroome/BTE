78b061edbf59aa087ffa491419c5c94099890183
Jakob McBroome <jmcbroome@gmail.com>
2022-04-02 12:49:46 -0700

    rename package import to bte instead of mat

--- run_test.py
+++ run_test.py
@@ -1,17 +1,17 @@
-import mat
+import bte
 import unittest
 
 class TestMat(unittest.TestCase):
     def test_ps(self):
-        t = mat.MATree("test.pb")
+        t = bte.MATree("test.pb")
         ps = t.get_parsimony_score()
         self.assertTrue(ps > 0)
     def test_dfe(self):
-        t = mat.MATree("test.pb")
+        t = bte.MATree("test.pb")
         nodes = t.depth_first_expansion()
         self.assertTrue(len(nodes) > 0)
     def test_node(self):
-        t = mat.MATree("test.pb")
+        t = bte.MATree("test.pb")
         root = t.get_root()
         children = root.children
         self.assertTrue(len(children) > 0)
